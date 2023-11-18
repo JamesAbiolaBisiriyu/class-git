d64e71d4b50f78087dcc5ccde37648f89a89a28e
james <emperor2g@gmail.com>
2023-11-18 17:04:07 +0100

    ES6class

--- git.js
+++ git.js
@@ -1,1 +1,92 @@
-let git = true
+// const Person = {
+//   firstName: 'James',
+//   lastName: 'Bisiryu',
+//   email: 'Emperor2g@gmail.com',
+//   course: 'Frontend Web Development',
+//   phone: 8171575268,
+//   married: true,
+//   healthStatus: undefined,
+//   hobbies:['coding','fishing','sleeping']
+// }
+// // console.log(Person.course);
+
+// Person.married = false
+// console.log(Person);
+
+
+// let age = 25;
+// console.log(age);
+// age = 32;
+// console.log(age);
+// const firstName = 'James'
+// console.log(firstName);
+// firstName = 'Bisiriyu';
+// console.log(firstName);
+
+// const firstName = 'James'
+// const job = 'Developer   '
+// const level = 'Senior  '
+// const nodw = 28 
+// const ppd = 5000
+// const expression = `mr ${firstName} will earn ${nodw*ppd} this November`
+// const sentence = 'Mr   ' + firstName + '  is an awesome ' + level  + job
+// console.log(sentence);
+// const newSentence = `Mr ${firstName} is an awesome ${level}${job}`
+// console.log(newSentence);
+// console.log(`
+// Gomycode
+// React Js
+// Node JS
+// Mongo DB
+// `);
+// console.log(expression);
+
+// const age = 32
+// const inecChecker = (age<18) ? console.log('You are not eligible to vote') : console.log('You are eligible to vote');
+// console.log(inecChecker);
+
+// const age = 17
+// const ozoneTicketing = (age<20) ? console.log('Your Bill is 5000') :  console.log('Your bill is 20000');
+// console.log(ozoneTicketing);
+// const age = 70
+// const silverBird = (age>=1 && age<=10) ? console.log('Ticket is free') : 
+//                    (age>=11 && age<=20) ? console.log('Ticket is 2000'):
+//                    (age>=21 && age<=30) ? console.log('ticket is 5000'):
+//                    (age>=31 && age<=50) ? console.log('Ticket is 10000'):
+//                    console.log('Ticket is 1000');
+
+// const age = 100
+// const palliative = (age>=1 && age<=10) ? console.log('Palliative is 5000'):
+//                     (age>=11 && age <=20) ? console.log('Palliative is 10000'):
+//                     (age>=21 && age <=36) ? console.log('Palliative is 20000'):
+//                     (age>=37 && age <=55) ? console.log('Palliative is 50000'):
+//                     (age>=56 && age <=70) ? console.log('Palliative is 70000'):
+//                     console.log('Palliative is 100000');
+
+// const sayHello = (name)=>{
+//   return `hello,${name}`;
+
+// }
+// sayHello('Victor')
+// console.log(sayHello('Victor'));
+
+// const profit = (cp,sp)=>{
+//   return sp-cp;
+// }
+// const answer = profit(10000,30000)
+// console.log(answer);
+
+// const perprofit = (cp,sp)=>{
+//   return ((sp - cp)/cp)*100;
+// }
+// const answer = perprofit(10000,50000)
+// console.log(answer);
+const cube = (number)=>{
+  return number**3
+}
+
+const cubepro = number => number**3 
+const answer = cube(12)
+const answer1 = cubepro(12)
+console.log(answer);
+console.log(answer1);
